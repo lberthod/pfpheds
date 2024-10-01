975a36f5f2133a310cd1ff48f00016c3a9ad393a
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2024-10-01 20:42:33 +0200

    ajout bouton

--- src/components/Institutions/InstitutionView.vue
+++ src/components/Institutions/InstitutionView.vue
@@ -3,16 +3,16 @@
   <div class="surface-card p-4 shadow-2 border-round">
     <div class="flex justify-content-between flex-column-reverse md:flex-row align-items-center ml-8">
       <div>
-        <div class="flex align-items-center text-900 font-medium text-6xl mb-2 pl-8 pr-8 ">
+        <div class="flex align-items-center text-900 font-medium text-6xl mb-2 pl-8 pr-8">
           {{ institutionDetails ? institutionDetails.Name : 'Chargement...' }}
         </div>
         <span class="text-900 font-bold text-3xl mb-4 mt-2 pl-8 pr-8">
-          <strong> {{ institutionDetails ? institutionDetails.Locality : '' }} </strong> - {{ institutionDetails ? institutionDetails.Address : '' }}
+          <strong>{{ institutionDetails ? institutionDetails.Locality : '' }}</strong> - {{ institutionDetails ? institutionDetails.Address : '' }}
         </span>
         <div class="flex flex-wrap justify-content-center md:justify-content-start gap-3 mt-2 pl-8 pr-8">
           <span class="inline-flex align-items-center py-2 px-3 font-medium border-1 surface-border border-round">
             <i class="pi pi-comments text-primary mr-2"></i>
-            <span class="text-900"> Langue : {{ institutionDetails ? institutionDetails.Language : '' }}</span>
+            <span class="text-900">Langue : {{ institutionDetails ? institutionDetails.Language : '' }}</span>
           </span>
           <span class="inline-flex align-items-center py-2 px-3 font-medium border-1 surface-border border-round">
             <i class="pi pi-comments text-primary mr-2"></i>
@@ -24,7 +24,7 @@
     <div class="text-center my-4">
       <img :src="institutionDetails ? institutionDetails.ImageURL : 'https://eduport.webestica.com/assets/images/courses/4by3/21.jpg'" alt="Institution Image" class="w-100px institution-image shadow">
     </div>
-    <div class="grid mb-4 justify-content-center ">
+    <div class="grid mb-4 justify-content-center">
       <div class="col-8 lg:col-5">
         <TabView>
           <TabPanel header="Informations générales de l'institution">
@@ -62,8 +62,10 @@
                   </div>
 
                   <!-- Bouton pour ouvrir le PDF -->
-                  <Button v-if="pdfUrl" label="Ouvrir le PDF" icon="pi pi-file-pdf" @click="openPDF" class="p-button-raised p-button-danger mt-4" />
-                  <p v-else>Aucun PDF disponible pour cette institution.</p> <!-- Message alternatif -->
+                  <div class="mt-4">
+                    <Button v-if="institutionDetails?.CyberleanURL" label="Ouvrir le PDF" icon="pi pi-file-pdf" @click="openPDF" class="p-button-raised p-button-danger" />
+                    <p v-else>Aucun PDF disponible pour cette institution.</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -81,40 +83,35 @@
 </template>
 
 <script>
-import { db, storage } from '../../../firebase.js';
+import { db } from '../../../firebase.js';
 import { ref as firebaseRef, onValue } from "firebase/database";
-import { getDownloadURL, ref as storageRef } from 'firebase/storage';
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css'; // Ensure Leaflet's CSS is included
+import 'leaflet/dist/leaflet.css';
 import Navbar from '@/components/Utils/Navbar.vue';
 import Footer from '@/components/Utils/Footer.vue';
 import Button from '@/views/uikit/Button.vue';
-import { ref } from 'vue';
-
-const liked = ref(false);
 
 export default {
   name: 'InstitutionView',
   components: {
-    Button,
     Navbar,
     Footer
   },
   data() {
     return {
-      institutionDetails: null,
-      map: null,
-      marker: null,
-      pdfUrl: null, // URL for the PDF file
+      institutionDetails: null, // Contient les détails de l'institution
+      map: null, // Carte Leaflet
+      marker: null, // Marqueur sur la carte
     };
   },
   mounted() {
-    this.fetchInstitutionDetailsFromFirebase();
+    this.fetchInstitutionDetailsFromFirebase(); // Charger les détails de l'institution
   },
   methods: {
+    // Initialiser la carte avec les coordonnées latitude et longitude
     initMap(lat, lng) {
       if (this.map) {
-        this.map.remove(); // Ensures the map is not initialized more than once
+        this.map.remove(); // Si la carte existe déjà, on la retire
       }
       this.map = L.map('map', {
         center: [lat, lng],
@@ -125,8 +122,10 @@ export default {
       }).addTo(this.map);
       this.marker = L.marker([lat, lng]).addTo(this.map);
     },
+
+    // Récupérer les détails de l'institution depuis Firebase
     fetchInstitutionDetailsFromFirebase() {
-      const institutionId = this.$route.params.id; // Assuming this is how you get the ID
+      const institutionId = this.$route.params.id; // ID de l'institution dans l'URL
       const institutionRef = firebaseRef(db, `Institutions/${institutionId}`);
 
       onValue(institutionRef, (snapshot) => {
@@ -134,36 +133,22 @@ export default {
           const data = snapshot.val();
           this.institutionDetails = data;
 
-          // Initialize map if coordinates are available
+          // Initialiser la carte si les coordonnées sont disponibles
           this.initMapIfNeeded();
-
-          // Fetch the PDF file if it exists in the institution details
-          if (data.pdfPath) {
-            this.fetchPDF(data.pdfPath);
-          } else {
-            console.log('No PDF path found in the institution data');
-          }
         } else {
-          this.$router.push({ name: 'Error404' });
+          this.$router.push({ name: 'Error404' }); // Redirection si l'institution n'existe pas
         }
       });
     },
-    fetchPDF(pdfPath) {
-      const pdfRef = storageRef(storage, pdfPath);
-      getDownloadURL(pdfRef)
-        .then((url) => {
-          this.pdfUrl = url;
-          console.log('PDF URL fetched:', this.pdfUrl); // Log the fetched URL
-        })
-        .catch((error) => {
-          console.error('Erreur lors de la récupération du PDF :', error);
-        });
-    },
+
+    // Ouvrir le PDF dans un nouvel onglet
     openPDF() {
-      if (this.pdfUrl) {
-        window.open(this.pdfUrl, '_blank');
+      if (this.institutionDetails?.CyberleanURL) {
+        window.open(this.institutionDetails.CyberleanURL, '_blank');
       }
     },
+
+    // Initialiser la carte si les coordonnées (Latitude, Longitude) sont disponibles
     initMapIfNeeded() {
       if (this.institutionDetails && this.institutionDetails.Latitude && this.institutionDetails.Longitude) {
         this.initMap(parseFloat(this.institutionDetails.Latitude), parseFloat(this.institutionDetails.Longitude));
@@ -182,10 +167,10 @@ export default {
   object-fit: cover;
   margin: auto;
   border-radius: 0.5rem;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Add shadow to the image */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
 
 .shadow {
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Add shadow to the map */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
 </style>
