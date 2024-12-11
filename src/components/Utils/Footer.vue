7cf4b82c9a13261b57fcc84f998aab07ed9d2b42
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2024-12-11 11:17:55 +0100

    footer

--- src/components/Utils/Footer.vue
+++ src/components/Utils/Footer.vue
@@ -3,7 +3,7 @@
     <div class="relative overflow-hidden flex flex-column justify-content-center">
     <div class="container mt-5 mb-5">
       <div class="col-lg-12 text-center">
-        <p>Copyright © 2023 <a href="https://www.hevs.ch">Theme HEdS</a> Développé <i class="fa fa-code"></i> avec la <i class="fa fa-heart"></i> par <a href="http://www.hevs.ch"> HES-SO Valais/Wallis - Haute École de Santé </a>.</p>
+        <p>Copyright © 2024 <a href="https://www.hevs.ch" class="text-primary">Theme HEdS</a> Développé <i class="pi pi-code text-primary"></i> avec le <i class="pi pi-heart-fill text-primary"></i> par la <a href="http://www.hevs.ch" class="text-primary"> HES-SO Valais/Wallis - Haute École de Santé </a></p>
       </div>
     </div>
     </div>
